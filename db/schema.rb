e783c0cc47a834e5de2b3e814cd953eb4ea7f7be
Michael Chong <mikec0920@gmail.com>
2023-03-23 11:17:32 -0700

    Create Posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_175242) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_181223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_175242) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
