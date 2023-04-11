e0fb8557c6161778b9576931420f85c3e096984d
Michael Chong <mikec0920@gmail.com>
2023-04-11 16:28:51 -0700

    Add comments reference to likes tables

--- db/migrate/20230323185338_create_likes.rb
+++ db/migrate/20230323185338_create_likes.rb
@@ -2,7 +2,8 @@ class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :post, null: true, foreign_key: true
+      t.references :comments, null: true, foreign_key: true
 
       t.timestamps
     end
