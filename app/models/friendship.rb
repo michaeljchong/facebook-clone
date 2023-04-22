7f222538db6a7dce97ae370ec3915a5016ae3436
Michael Chong <mikec0920@gmail.com>
2023-04-21 23:09:51 -0700

    Create scope to filter friends

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,7 @@
 class Friendship < ApplicationRecord
   belongs_to :requester, class_name: "User", foreign_key: :requester_id
   belongs_to :requestee, class_name: "User", foreign_key: :requestee_id
+
+  scope :friends, -> { where('accepted = ?', true) }
+  scope :not_friends, -> { where{'accepted = ?', false} }
 end
