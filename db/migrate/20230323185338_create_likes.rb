deda9dfe602c211e8423fad1ea538f18e063a993
Michael Chong <mikec0920@gmail.com>
2023-05-04 15:21:50 -0700

    Remove foreign key field

--- db/migrate/20230323185338_create_likes.rb
+++ db/migrate/20230323185338_create_likes.rb
@@ -2,7 +2,7 @@ class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :likeable, polymorphic: true, null: true, foreign_key: true
+      t.references :likeable, polymorphic: true, null: true
 
       t.timestamps
     end
