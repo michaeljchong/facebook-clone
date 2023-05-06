beceffb3b89c379833da255c1a8a1f014aeccb5e
Michael Chong <mikec0920@gmail.com>
2023-05-05 17:16:28 -0700

    Update index and create to be user specific

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = current_user.visible_posts
   end
 
   # GET /posts/1 or /posts/1.json
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
 
     respond_to do |format|
       if @post.save
