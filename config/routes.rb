3b176ab88396082ded6a94a3085f459fb643755e
Michael Chong <mikec0920@gmail.com>
2023-05-25 17:16:02 -0700

    Nest associated resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  get 'friendships/create'
-  devise_for :users
-  resources :comments
-  resources :likes
-  resources :posts
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "posts#index"
+  devise_for :users
+
+  resources :users do
+    resources :friendships
+  end
+  
+  resources :posts do
+    resources :comments
+  end
+
+  resources :comments do
+    resources :likes
+  end
 end
