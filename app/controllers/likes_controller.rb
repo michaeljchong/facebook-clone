cad0bf27f00dd842a8f6f5057bff6b2ac91ccf77
Michael Chong <mikec0920@gmail.com>
2023-05-25 21:01:12 -0700

    Clean up likes views and methods

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,4 @@
 class LikesController < ApplicationController
-  before_action :set_like, only: %i[ show edit update destroy ]
-
-  # GET /likes or /likes.json
-  def index
-    @likes = Like.all
-  end
-
-  # GET /likes/1 or /likes/1.json
-  def show
-  end
-
-  # GET /likes/new
-  def new
-    @like = Like.new
-  end
-
-  # GET /likes/1/edit
-  def edit
-  end
-
   # POST /likes or /likes.json
   def create
     @like = Like.new(like_params)
@@ -34,21 +14,9 @@ class LikesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /likes/1 or /likes/1.json
-  def update
-    respond_to do |format|
-      if @like.update(like_params)
-        format.html { redirect_to like_url(@like), notice: "Like was successfully updated." }
-        format.json { render :show, status: :ok, location: @like }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /likes/1 or /likes/1.json
   def destroy
+    @like = Like.find(params[:id])
     @like.destroy
 
     respond_to do |format|
@@ -58,11 +26,6 @@ class LikesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_like
-      @like = Like.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def like_params
       params.require(:like).permit(:user_id, :post_id)
