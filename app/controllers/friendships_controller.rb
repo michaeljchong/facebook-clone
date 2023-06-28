72f6971eb5f2227053636cb666cdbedc0ff8e15a
Michael Chong <mikec0920@gmail.com>
2023-06-28 15:33:15 -0700

    Implement one-way adding friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
+    @friendship = current_user.friend_requester.new(requestee_id: params[:user_id], accepted: true)
+
+     if @friendship.save
+      flash[:success] = 'Friend Request Sent!'
+    else
+      flash[:danger] = 'Friend Request Failed!'
+    end
+
+    redirect_to root_path
   end
 end
