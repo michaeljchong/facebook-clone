fc6306305bf022fe77ccd4b128b9ee1074a1acaf
Michael Chong <mikec0920@gmail.com>
2023-06-29 10:50:13 -0700

    Implement destroy method

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,23 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = current_user.friend_requester.new(requestee_id: params[:user_id], accepted: true)
+    @friendship = current_user.friend_requester.new(requestee_id: params[:user_id])
 
-     if @friendship.save
+    if @friendship.save
       flash[:success] = 'Friend Request Sent!'
     else
       flash[:danger] = 'Friend Request Failed!'
     end
 
     redirect_to root_path
+  end
+
+  def destroy
+    @friendship = Friendship.find_by(id: params[:id])
+    @friendship.destroy
+
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: "Friendship was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 end
