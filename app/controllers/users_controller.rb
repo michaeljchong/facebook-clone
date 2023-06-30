d96735929806f929d7472ef1c7ede7245f711e9f
Michael Chong <mikec0920@gmail.com>
2023-06-29 18:09:16 -0700

    Display friends section and allow unfriending

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all
+    @friends = current_user.friends
     @sent_requests = current_user.sent_requests
     @received_requests = current_user.received_requests
   end
