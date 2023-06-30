faa80e9ef283d1b0829115497656a3c9ddd6f70c
Michael Chong <mikec0920@gmail.com>
2023-06-29 18:07:58 -0700

    Implement unfriend method

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -39,4 +39,16 @@ class FriendshipsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  def unfriend
+    @friendship = Friendship.find_by(requester_id: current_user.id, requestee_id: params[:user_id])
+    @friendship.destroy
+    @reciprocal_friendship = Friendship.find_by(requestee_id: current_user.id, requester_id: params[:user_id])
+    @reciprocal_friendship.destroy
+
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: "Successfully unfriended." }
+      format.json { head :no_content }
+    end
+  end
 end
