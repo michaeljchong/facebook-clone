af63e4bbd5b020d82792a5f705343acaf665c0c1
Michael Chong <mikec0920@gmail.com>
2023-07-02 10:41:23 -0700

    Create scopes to filter notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :user
+
+  scope :requests, -> { where(category: "request") }
+  scope :comments, -> { where(category: "comment") }
+  scope :likes, -> { where(category: "like") }
 end
