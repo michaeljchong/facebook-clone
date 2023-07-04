aab1e623bb1d3ffe55cd0c0afb9d68a3c93d7f48
Michael Chong <mikec0920@gmail.com>
2023-07-03 17:13:38 -0700

    Display user notifications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,6 @@ class UsersController < ApplicationController
   # GET /users/1 or /users/1.json
   def show
     @user = User.find(params[:id])
+    @notifications = @user.notifications
   end
 end
