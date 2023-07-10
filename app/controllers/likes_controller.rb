26c5a8c3176d5ee4bcc9ff1bdbc6213399572b4d
Michael Chong <mikec0920@gmail.com>
2023-07-10 12:26:36 -0700

    Implement destroy/unlike method and update flash notices

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,25 +1,25 @@
 class LikesController < ApplicationController
-  # POST /likes or /likes.json
   def create
     @like = current_user.likes.new(like_params)
 
-    respond_to do |format|
-      if @like.save
-        format.html { redirect_to @like.likeable, notice: "Like was successfully created." }
-      end
+    if @like.save
+      flash[:success] = "Like was successfully created."
+    else
+      flash[:danger] = 'Like Failed!'
     end
+    redirect_to @like.likeable
   end
 
-  # DELETE /likes/1 or /likes/1.json
   def destroy
-    @like = current_user.likes.find(params[:id])
+    @like = Like.find(params[:id])
     likeable = @like.likeable
-    @like.destroy
 
-    respond_to do |format|
-      format.html { redirect_to likeable, notice: "Like was successfully destroyed." }
-      format.json { head :no_content }
+    if @like.destroy
+      flash[:success] = "Like was successfully destroyed."
+    else
+      flash[:danger] = 'Unlike Failed!'
     end
+    redirect_to likeable
   end
 
   private
