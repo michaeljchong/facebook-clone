bed75b3f3c51066ed1a854e0878c12235e3532a6
Michael Chong <mikec0920@gmail.com>
2023-07-19 22:11:47 -0700

    Refactor time created to use formatted time helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def new_notification(user_id, notification_id, category)
     User.find(user_id).notifications.create(notification_id: notification_id, category: category)
   end
+
+  def formatted_time(item)
+    item.created_at.strftime("%b %-d, %Y at %-I:%M %p")
+  end
 end
