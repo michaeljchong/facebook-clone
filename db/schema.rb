3e3ba6bc078167ea8cb89ee6fb8470dfc4bfa0fd
Michael Chong <mikec0920@gmail.com>
2023-07-21 22:16:12 -0700

    Set default user profile image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_21_193307) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_22_050144) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,7 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_193307) do
     t.datetime "remember_created_at"
     t.string "first_name"
     t.string "last_name"
-    t.string "profile_img"
+    t.string "profile_img", default: "default-profile-icon-24.jpg"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
